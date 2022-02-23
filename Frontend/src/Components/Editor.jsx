d1caef53b2a5164e005e62cbe7ac9ba97ab73fa2
areebkhan11 <areebkhan123123@gmail.com>
2022-02-24 00:35:05 +0500

    Worked on ui

--- Frontend/src/Components/Editor.jsx
+++ Frontend/src/Components/Editor.jsx
@@ -1,19 +1,46 @@
+import React from 'react';
 import {useEffect} from'react';
 import Quill from 'quill'
 import 'quill/dist/quill.snow.css'
 import {Box} from '@mui/material'
+import styled from '@emotion/styled'
+
+const Component = styled.div`
+    background: #F5F5F5;
+`
+
+const toolbarOptions = [
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+  
+    [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+    [{ 'indent': '-1'}, { 'indent': '+1' }],           // outdent/indent
+    [{ 'direction': 'rtl' }],                         // text direction
+  
+    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+  
+    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+    [{ 'font': [] }],
+    [{ 'align': [] }],
+  
+    ['clean']                                         // remove formatting button
+  ];
 
 
 const Editor =  () => { 
 
     useEffect (()=>{
-        const quilServer = new Quill('#container', {theme: 'snow'});
+        const quilServer = new Quill('#container', {theme: 'snow' , modules:{toolbar: toolbarOptions}});
     },[])
 
 return(
-    <Box id= "container">
 
-    </Box>
+    <Component>
+    <Box className='container' id= "container"></Box>
+    </Component>
 )
 }
 
